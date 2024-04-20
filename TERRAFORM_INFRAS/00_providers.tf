4c7bb6a37376b6a243bc0fa0b1ae4eca1627d49a
bakarySeydouDiaby <bakaryseydou@gmail.com>
2024-04-20 03:54:04 +0200

    [terraform] update 00_providers.tf to set remote state backend in github

--- TERRAFORM_INFRAS/00_providers.tf
+++ TERRAFORM_INFRAS/00_providers.tf
@@ -6,4 +6,12 @@ terraform {
       version = "~> 1.52.1" #version du provider
     }
   }
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    #username       = "https://github.com/bakarySeydouDiaby/terraform_openstack_infomaniak" 
+  }
 }
